6a1f64948aafb227118900622fb59800955d5bcb
canaryGrapher <yasharyan307@outlook.com>
2023-12-27 15:29:55 +0530

    values

--- src/invokation_options.js
+++ src/invokation_options.js
@@ -1,3 +1,17 @@
+
+
+// Function to convert Base64-encoded string to ArrayBuffer
+function base64ToArrayBuffer() {
+    const base64String = localStorage.getItem("publicKey")
+    const binaryString = atob(base64String);
+    const len = binaryString.length;
+    const bytes = new Uint8Array(len);
+    for (let i = 0; i < len; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
+
 const registrationOptions = {
     publicKey: {
         challenge: new Uint8Array(32), // Replace with actual challenge
@@ -6,7 +20,7 @@ const registrationOptions = {
             name: "ICICI Bank Limited"
         },
         user: {
-            id: new Uint8Array([12, 13, 14, 15]),
+            id: new Uint8Array(16), // Optional
             name: "john.doe@example.com",
             displayName: "John Doe"
         },
@@ -34,7 +48,7 @@ const loginOptions = {
         },
         allowCredentials: [{
             type: 'public-key',
-            id: new Uint8Array([12, 13, 14, 15]), // Optional
+            id: new Uint8Array(base64ToArrayBuffer()), // Optional
         }],
         authenticatorSelection: {
             // only stick to device specific authentication like Windows Hello / TouchID / FingerPrint authentication
--- src/registration/index.js
+++ src/registration/index.js
@@ -1,11 +1,17 @@
 import { registrationOptions } from "../invokation_options"
 
+function arrayBufferToBase64(buffer) {
+    const binary = new Uint8Array(buffer);
+    return btoa(String.fromCharCode.apply(null, binary));
+}
+
 const handleRegistration = () => {
     navigator.credentials.create(registrationOptions)
         .then((newCredentialInfo) => {
             // Handle successful WebAuthn registration
             console.log("WebAuthn registration done: ", newCredentialInfo)
             localStorage.setItem("registrationDone", "true")
+            localStorage.setItem("publicKey", arrayBufferToBase64(newCredentialInfo.rawId))
             console.log(newCredentialInfo);
             // alert("Registration done, data to be sent to server!")
         })
